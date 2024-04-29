95e50996853bd067a0baf3fbbd6a2e15c2b57d17
Saxon Chen <sxa05z@gmail.com>
2024-04-29 19:07:08 +0800

    finish section2

--- main.rs
+++ main.rs
@@ -1,32 +1,6 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
-
 fn main() {
-    println!("Guess the number!");
+    let mut spaces = "   ";
+    let spaces: u32 = spaces.parse().len();
 
-    let secret_number = rand::thread_rng()
-                                    .gen_range(1..=100);
-
-    println!("The secret number is: {secret_number}");
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: i32 = guess.trim()
-                            .parse()
-                            .expect("Please type a number!");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-    }
+    println!("x 的數值為：{spaces}");
 }
